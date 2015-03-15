cca9d5e021bf2ace40ec7d75b6cd27cebd622586
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2015-03-15 21:55:52 +0000

    Stable version

--- index.js
+++ index.js
@@ -1,38 +1,70 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var xray = require('x-ray');
 var router = express.Router();
+var _ = require('lodash');
 
-router.post('/', function (req, res) {
+router
+  .post(bodyParser.json())
+  .post('/', function (req, res) {
 
-  var x; // x-ray instance
-  var url = req.params.url;
-  var select = req.params.select;
+    var x; // x-ray instance
+    var url = req.body.url;
+    var select = req.body.select;
+    var prepare = req.body.prepare;
 
-  if(!url) {
-    res
-      .status(400)
-      .error({
-        error: 'Please provide "url" property.'
+    if(!url) {
+      res
+        .status(400)
+        .send({
+          error: 'url property missing'
+        });
+      return;
+    }
+
+    if(!select) {
+      res
+        .status(400)
+        .send({
+          error: 'select property missing'
+        });
+      return;
+    }
+
+    x = xray(url);
+    x.select(select);
+
+    if(prepare) {
+      _.each(prepare, function (regex, name) {
+        x.prepare(name, function (str) {
+          var m = str.match(new RegExp(regex));
+          if(m && m.length > 1) {
+            m.shift();
+            return m.length > 1 ? m : m[0];
+          }
+          return str;
+        });
       });
-    return;
-  }
+    }
 
-  if(!select) {
-    res
-      .status(400)
-      .error({
-        error: 'Please provide "select" property.'
-      });
-    return;
-  }
+    x.prepare({
+      number: function (str) {
+        return Number(str);
+      },
+      boolean: function (str) {
+         return !!str;
+      }
+    });  
 
-  x = xray(url);
-  x.select(select);
+    x.run(function(err, data) {
+      if(err) {
+        res
+          .status(500)
+          .send(err);
+      }
+      res.send(data);
+    });
 
-  x.run(err, function(data) {
-    res.send(data);
   });
-
-});
 
 module.exports = router;
